df567dd50c8797fd334e8d5cb41a6f95d09c5221
xiaoyuanpang <46011876+xiaoyuanpang@users.noreply.github.com>
2023-12-25 12:29:28 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -17,7 +17,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 
 # 添加额外插件
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
+git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 git clone --depth=1 -b openwrt-18.06 https://github.com/selfcan/luci-app-homebox package/luci-app-homebox
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
